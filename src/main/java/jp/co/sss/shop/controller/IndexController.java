fc332ed6084d558469e1f6b97669353088448bcb
tis2023 <fh.tis2023@gmail.com>
2023-04-25 16:46:13 +0900

    first commit

---
+++ src/main/java/jp/co/sss/shop/controller/IndexController.java
@@ -0,0 +1,12 @@
+package jp.co.sss.shop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+    @RequestMapping(path = "/")
+    public String index(){
+        return "index";
+    }
+}
